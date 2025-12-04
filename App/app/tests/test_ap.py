06be60f0e53867f8ee2bc0e77c0b26704e64a9e6
Anna Ransom <36892855+AnnaRansom@users.noreply.github.com>
2025-12-03 22:27:20 -0500

    First API test as unittest

--- App/app/tests/test_ap.py
+++ App/app/tests/test_ap.py
@@ -3,4 +3,14 @@ from app import app, import_meal_from_mealdb
 from app.db import get_db_connection
 
 # API tests
+def test_recipe_list_route():
+    """
+    /api/recipes should return HTTP 200 (OK)
+    Response body should be a JSON list
+    """
+    client = app.test_client()
+    resp = client.get("/api/recipes")
 
+    assert resp.status_code == 200
+    data = resp.get_json()
+    assert isinstance(data, list)
